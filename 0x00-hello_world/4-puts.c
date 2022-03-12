ded6e6306fa0165cbb0d4ef55124b91e74f3be14
OmotoshoTomisin <omotoshooluwatosin2017@gmail.com>
2022-03-12 12:46:21 -0800

    My commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise 
+ * main - print the phrase "programming is like building a multilingual puzzle
+ * Return 0 if exited properly, non-zero otherwise
  */
-
-
 int main(void)
 {
         puts("\"programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }
