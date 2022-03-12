bd35feb3a4a3e2a36e2d09c633b37fb0aa428757
OmotoshoTomisin <omotoshooluwatosin2017@gmail.com>
2022-03-12 15:03:35 -0800

    My commit message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - Entry point
  * Return: Always 1 (success)
  */
-int (void)
+int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
 						\" - Dora Korpar, 2015-10-19\n",59)
