3564f4f633a7f3ba923713f9628f9f09a5a7fc91
OmotoshoTomisin <omotoshooluwatosin2017@gmail.com>
2022-03-13 08:02:12 -0700

    My commit message

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("size of char: %d byte(s)\n", sizeof(char));
+		printf("size of char: %d byte(s)\n", sizeof(int));
+			printf("size of char: %d byte(s)\n", sizeof(long int));
+				printf("size of char: %d byte(s)\n", sizeof(float));
+					return (0);
+}
