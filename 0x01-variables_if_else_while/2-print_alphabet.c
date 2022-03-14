2f1889ebeead3bab01b8dc718747ed0efd74f41f
OmotoshoTomisin <omotoshooluwatosin2017@gmail.com>
2022-03-13 18:21:14 -0700

    My commit message

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * 
  * Return: Always 0 (Success/correct)
  */
 int main(void)
-{
-char alphabet;
 
-for (alphabet = 'a'; ch <= 'z'; alphabet++)
+{
+char alphabet:
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(alphabet);
 }
+
 putchar('\n');
+
 return (0);
 }
