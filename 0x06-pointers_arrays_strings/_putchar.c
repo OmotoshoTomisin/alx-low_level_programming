902e677651f35b0402996d3c3a7eebae306abd61
OmotoshoTomisin <omotoshooluwatosin2017@gmail.com>
2022-03-24 16:37:08 +0100

    my commit

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,12 +1,12 @@
 
-/**
+/*
  * _putchar - writes the character c toto stdout
  * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and error is set
  */
- int _putchar(char c)
- {
- return (write(1, &c, 1));
- }
+int _putchar (char c)
+{
+return (write(1, &c, 1));
+}
